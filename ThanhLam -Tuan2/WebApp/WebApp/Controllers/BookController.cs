40ec35479e7c17049e2ea6370c5ba279414915db
thanhlam1702 <thanhlam17021999@gmail.com>
2020-05-28 15:58:42 +0700

    tuan 2 lap trinh web

---
+++ ThanhLam -Tuan2/WebApp/WebApp/Controllers/BookController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class BookController : Controller
+    {
+        
+        // GET: Book
+        public string HelloTeacher()
+        {
+            return "Hello Nguyen Dinh Anh";
+        }
+        public ActionResult ListBook()
+        {
+            var books = new List<string>();
+            books.Add("Book2");
+            books.Add("Book2");
+            books.Add("Book3");
+            ViewBag.Books = books;
+            return View();
+        }
+        public ActionResult ListBookModel()
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "Book 1", "Tran Thi Deo", "/Content/Images/book1.jpg"));
+            books.Add(new Book(2, "Book 2", "NguyenThi Hanh Phuc", "/Content/Images/book2.jpg"));
+            books.Add(new Book(3, "Book 3", "Nguyen Van Teo", "/Content/Images/book3.jpg"));
+            return View(books);
+        }
+
+        public ActionResult EditBook(int? id)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "Book 1", "Tran Thi Deo", "/Content/Images/book1.jpg"));
+            books.Add(new Book(2, "Book 2", "NguyenThi Hanh Phuc", "/Content/Images/book2.jpg"));
+            books.Add(new Book(3, "Book 3", "Nguyen Van Teo", "/Content/Images/book3.jpg"));
+            Book book = new Book();
+            foreach (Book b in books)
+            {
+                if (b.Id == id)
+                {
+                    book = b;
+                    break;
+                }
+            }
+            if (book == null) return HttpNotFound();
+            return View(book);
+        }
+        [HttpPost, ActionName("EditBook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditBook(int? id, string Title, string Author, string ImageCover)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "Book 1", "Tran Thi Deo", "/Content/Images/book1.jpg"));
+            books.Add(new Book(2, "Book 2", "NguyenThi Hanh Phuc", "/Content/Images/book2.jpg"));
+            books.Add(new Book(3, "Book 3", "Nguyen Van Teo", "/Content/Images/book3.jpg"));
+            if (id == null) return HttpNotFound();
+            Book book = new Book();
+            foreach (Book b in books)
+            {
+                if (b.Id == id)
+                {
+                    b.Title = Title;
+                    b.Author = Author;
+                    b.ImageCover = ImageCover;
+                    break;
+                }
+            }
+            return View("ListBookModel", books);
+        }
+        public ActionResult CreateBook()
+        {
+            return View();
+        }
+        [HttpPost, ActionName("CreateBook")]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Contact([Bind(Include = "Id, Title, Author, ImageCover")]Book book)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "Book 1", "Tran Thi Deo", "/Content/Images/book1.jpg"));
+            books.Add(new Book(2, "Book 2", "NguyenThi Hanh Phuc", "/Content/Images/book2.jpg"));
+            books.Add(new Book(3, "Book 3", "Nguyen Van Teo", "/Content/Images/book3.jpg"));
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    books.Add(book);
+                }
+            }
+            catch (RetryLimitExceededException)
+            {
+                ModelState.AddModelError("", "Error Save Data");
+            }
+            return View("ListBookModel", books);
+        }
+
+    }
+}
